e2b74c360ed93c85d134ba562e5ff37341595f6a
nruip2 <n.ruizp2@uniandes.edu.co>
2023-11-04 18:49:15 -0500

    RFC4

--- src/main/java/uniandes/edu/co/proyecto/repository/ServicioRepository.java
+++ src/main/java/uniandes/edu/co/proyecto/repository/ServicioRepository.java
@@ -1,6 +1,7 @@
 package uniandes.edu.co.proyecto.repository;
 
 import java.util.Collection;
+import java.util.List;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Modifying;
@@ -32,4 +33,10 @@ public interface ServicioRepository extends JpaRepository<Servicio, Integer>{
     @Query(value = "INSERT INTO servicios (id, nombre,descripcion,costoporunidad,unidad,horario,tiposervicio,capacidad) VALUES ( serviciossecuencia.nextval , :nombre, :descripcion, :costoporunidad, :unidad, :horario, :tiposervicio, :capacidad)", nativeQuery = true)
     void insertarServicio(@Param("nombre") String nombre, @Param("descripcion") String descripcion, @Param("costoporunidad") Integer costoporunidad, @Param("unidad") String unidad ,@Param("horario") String horario, @Param("tiposervicio") String tiposervicio, @Param("capacidad") Integer capacidad);
 
+    @Query(value = "SELECT s.nombre, s.descripcion, s.costoporunidad, s.unidad, s.horario, s.tiposervicio, s.capacidad FROM servicios s LEFT JOIN consumos c ON s.id = c.servicios_id " +
+    "WHERE (:costo1 IS NULL OR (s.costoporunidad BETWEEN :costo1 AND :costo2)) " +
+    "AND (:fecha1 IS NULL OR (c.fechaconsumo BETWEEN to_date(:fecha1,'YYYY-MM-DD') AND to_date(:fecha2,'YYYY-MM-DD'))) " +
+    "AND (:usuario_id IS NULL OR c.usuarios_id = :usuario_id) " +
+    "AND (:tiposervicio IS NULL OR s.tiposervicio = :tiposervicio)", nativeQuery = true)
+    List<Object[]> RFC4(@Param("costo1") String costo1,@Param("costo2") String costo2, @Param("fecha1") String fecha1, @Param("fecha2") String fecha2,@Param("tiposervicio") String tiposervicio, @Param("usuario_id") String usuario_id);
 }
